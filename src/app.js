a94ec6b8bcaf462bbb1a1dc71fc84d5b1c16ecab
andersonvieira96 <andersonvieira96@hotmail.com>
2020-04-13 12:57:24 -0300

    Revert "entrega do desafio"
    This reverts commit 4b8a40f91d7ea6056809e9f7463bd0e98d841256.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid, isUuid } = require("uuidv4");
+// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,68 +10,24 @@ app.use(cors());
 
 const repositories = [];
 
-app.use('/repositories/:id', (request, response, next) =>{
-  const { id } = request.params;
-
-  if(!isUuid(id)) return response.status(400).send();
-
-  return next();
-})
-
-
 app.get("/repositories", (request, response) => {
-  return response.json(repositories)
+  // TODO
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body;
-
-  const project = {
-    id: uuid(),
-    title,
-    url,
-    techs,
-    likes: 0
-  }
-  repositories.push(project)
-
-  return response.json(project);
+  // TODO
 });
 
 app.put("/repositories/:id", (request, response) => {
-  const { title, url, techs } = request.body;
-  const { id } = request.params;
-
-  const project = repositories.find(proj => proj.id === id);
-  if (!project) return response.status(400).send();
-
-  project.title = title
-  project.url = url;
-  project.techs = techs;
-
-  return response.json(project);
+  // TODO
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-
-  const projectIndex = repositories.find(proj => proj.id === id);
-  if (projectIndex < 0 ) return response.status(400).send();
-
-  repositories.splice(projectIndex);
-
-  return response.status(204).send();
+  // TODO
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
-
-  const project = repositories.find(proj => proj.id === id);
-  if (!project) return response.status(400).send();
-
-  project.likes +=1;
-
-  return response.json(project);
+  // TODO
 });
 
 module.exports = app;
