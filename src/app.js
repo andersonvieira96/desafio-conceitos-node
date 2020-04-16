348e34cf1d6e69e8861cba88f78a5962250b347a
Anderson Vieira <anderson.vieira@original.com.br>
2020-04-16 14:36:48 -0300

    challenge delivery

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,68 @@ app.use(cors());
 
 const repositories = [];
 
+app.use('/repositories/:id', (request, response, next) =>{
+  const { id } = request.params;
+
+  if(!isUuid(id)) return response.status(400).send();
+
+  return next();
+})
+
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(project)
+
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const project = repositories.find(proj => proj.id === id);
+  if (!project) return response.status(400).send();
+
+  project.title = title
+  project.url = url;
+  project.techs = techs;
+
+  return response.json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.find(proj => proj.id === id);
+  if (projectIndex < 0 ) return response.status(400).send();
+
+  repositories.splice(projectIndex);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const project = repositories.find(proj => proj.id === id);
+  if (!project) return response.status(400).send();
+
+  project.likes +=1;
+
+  return response.json(project);
 });
 
 module.exports = app;
